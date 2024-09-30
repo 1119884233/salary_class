3592161ab615b47c96b4cf0abf5d91e5417058b5
1119884233 <1119884233@qq.com>
2024-09-30 16:53:21 +0800

    提交文档

--- salary_dis/data_get/data_get.py
+++ salary_dis/data_get/data_get.py
@@ -1,4 +1,25 @@
 #导入相应的库
 import requests
+
+import config.config
+from   config import *
+
 #获取源代码
-res = requests.get()
+def get_html(url,headers):
+    response = requests.get(url,headers)
+#更改编码方式
+    response.encoding = "jbk"
+    if response.status_code == 200:
+        return response.text
+    else:
+        print("获取源代码错误")
+
+def main():
+    url = config.url_html
+    headers = config.headers
+    html = get_html(url,headers)
+    print(html)
+
+if __name__ == '__main__':
+    main()
+
